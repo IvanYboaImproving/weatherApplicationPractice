9344cbde7492cc426cdddef6ffa5653e110c5d9c
Ivan Yboa <hector.yboa@improving.com>
2022-10-18 19:00:16 -0500

    commit with charts and install ReactRoutes

---
+++ src/pages/CityPage.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const CityPage = (props) => {
+  return (
+    <div>
+        City Page
+    </div>
+  )
+};
+
+CityPage.propTypes = {
+
+};
+
+export default CityPage;
---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const WelcomePage = (props) => {
+  return (
+    <div>
+        Welcome Page
+    </div>
+  )
+};
+
+WelcomePage.propTypes = {
+
+};
+
+export default WelcomePage;
